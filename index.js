7434532f5f118e19c80ead39709f354b3dd6858a
AliHafeez337 <alihafeez337@gmail.com>
2020-05-21 01:11:35 +0500

    8th - Admin can upload task pictures...

--- index.js
+++ index.js
@@ -111,6 +111,7 @@ app.use(function(req, res, next) {
 
 /* ROUTES */
 
+app.use('/taskImage', require("./routes/taskImage"));
 app.use('/task', require("./routes/task"));
 app.use('/userImage', require("./routes/userImage"));
 app.use('/admin', require('./routes/admin'));
